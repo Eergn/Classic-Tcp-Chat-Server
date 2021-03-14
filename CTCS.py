c16912a81e378c781f7e3a40e5c1c17d845fee6b
Eergn <58009226+Eergn@users.noreply.github.com>
2021-03-14 06:41:12 -0400

    Adding The Main Source File

---
+++ CTCS.py
@@ -0,0 +1,53 @@
+import threading
+import socket
+
+ServerHost = "127.0.0.1"
+Port = 497
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((ServerHost,Port))
+server.listen()
+
+clients = []
+usernames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = usernames[index]
+            broadcast(f'{username} left the chat'.encode('ascii'))
+            usernames.remove(username)
+            break
+
+
+def receive():
+    print("The Server Is Listening")
+    while True:
+        client, address = server.accept()
+        print(f"{str(address)}Connected")
+        client.send("Username? >> ".encode("ascii"))
+        try:
+            username = client.recv(1024).decode('ascii')
+        except:
+            username = "Guest User"
+        usernames.append(username)
+        clients.append(client)
+        print(f"User Name Is {username}")
+        broadcast( (username + " Joined The Chat! .\n").encode("ascii"))
+
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+receive()
